e378ae64bd9c4302db798e03f341db30d6baf0b1
MacBook Pro <almir.mustafic.tz@gmail.com>
2023-06-10 00:48:39 +0200

    password-removed-from-test-trait

--- app/Traits/TestTrait.php
+++ app/Traits/TestTrait.php
@@ -7,9 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 trait TestTrait {
 
-
-    protected $password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";
-
     
     public function testUser(): array
     {
